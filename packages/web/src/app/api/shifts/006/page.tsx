97cf1085822ac962dbd2cbf2a7f204cbf96e1230
Filipo <filgreen@pm.me>
2026-08-26 23:26:39 +0200

    Web: Make the easter egg page actually work this time

--- packages/web/src/app/api/shifts/006/page.tsx
+++ packages/web/src/app/api/shifts/006/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 const GIF006 = [
   "https://cdn.discordapp.com/attachments/1461796259613507614/1536067105542377562/togif.gif?ex=6a9076f8&is=6a8f2578&hm=1d2b86ab6c1af7f3f1deb14e6b8dbc47050c310909992a65dd351d615db45086&",
   "https://cdn.discordapp.com/attachments/1461796259613507614/1536067286505492512/togif.gif?ex=6a907723&is=6a8f25a3&hm=0a86676a728ad6eb7bd26ce6d0552f96d62c543323cb202b5de0ff257ace3f73&",
@@ -7,5 +9,11 @@ const GIF006 = [
 ];
 
 export default function Zmiana006NieIstnieje() {
-  return <img src={GIF006[Math.floor(Math.random() * 3)]} />;
+  const [random, setRandom] = useState<number>(0);
+
+  useEffect(() => {
+    setRandom(Math.floor(Math.random() * 3));
+  }, []);
+
+  return <img src={GIF006[random]} />;
 }
